98297b4aeea909510d5543722d49a9f4873bdfb4
BK-725 <80371033@qq.com>
2020-11-05 21:22:56 +0800

    完成登录功能

--- src/main.js
+++ src/main.js
@@ -1,8 +1,13 @@
 import Vue from 'vue'
 import App from './App.vue'
-import router from './router'
+import router from './router/index.js'
 import './plugins/element.js'
+// 导入全局样式表
+import './assets/css/global.css'
+import axios from 'axios'
 
+Vue.prototype.$http = axios
+axios.defaults.baseURL = 'http://127.0.0.1:8888/api/private/v1/'
 Vue.config.productionTip = false
 
 new Vue({
